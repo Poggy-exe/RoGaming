42d1f265da8f93b9402c8309f362aeaefa4dabda
T3CH <jlp02.dk@gmail.com>
2020-11-19 23:03:54 +0100

    Change embed look and fix more repeating questions

--- Commands/post.py
+++ Commands/post.py
@@ -4,7 +4,7 @@ import json
 import requests
 from datetime import datetime
 from secrets import * 
-from random import randint
+#from random import randint
 
 class Advertising(commands.Cog):
     def __init__(self, client):
@@ -20,7 +20,6 @@ class Advertising(commands.Cog):
     # Command
     @commands.command()
     async def post(self, ctx):
-        await ctx.send("Check your dm's a thread just started!")
 
         def Check(context):
             return context.author == ctx.author and isinstance(context.channel, discord.channel.DMChannel)
@@ -28,36 +27,38 @@ class Advertising(commands.Cog):
         def post(post : object):
             print(post)
 
-        async def Ask(ctx, data : object, index : int, post : object, max_index : int,timeout :int = 60, check : callable = Check, next_ : callable = None):
+        async def Ask(ctx, data : object, index : int, post : object, max_index : int,timeout : int = 120, check : callable = Check, next_ : callable = None):
             
             retry = lambda: Ask(ctx,data, index, post, max_index,timeout,check, next_)
 
             question = list(data["Questions"])[index]
 
-            embed_q = discord.Embed(title = data["Title"], description=data["Description"])
-            embed_q.add_field(name="Question", value=f"`{question['Question']}`")
-            embed_q.color = randint(0, 0xffffff)
+            embed_q = discord.Embed(title = data["Title"] if index == 0 else "", description=data["Description"] if index == 0 else "", color=discord.Color.from_rgb(253,253,253))
 
             #print("Choises" in question)
 
-            if("Example" in question):
-                embed_q.add_field(name="Example",value= f'`{question["Example"] if "Example" in question else "None provided"}`')
+            br = "\n"
+
+            embed_q.add_field(name=question['Question'],value= f'`{question["Example"] + br if "Example" in question else "None provided"}`', inline=False)
                 
-            if("Choises" in question):
-                embed_q.add_field(name="Choises",value=f'`{", ".join(question["Choises"]) if "Choises" in question else "Anything"}`')
-
             if("Format" in question):
-                embed_q.add_field(name="Format",value=f'`{question["Format"] if "Format" in question else "Anything"}`')
+                embed_q.add_field(name="Answer must be",value=f'`{question["Format"] if "Format" in question else "Anything"}`', inline=False)
 
+            embed_q.add_field(name="\u200b", value="\nUse `cancel` to cancel thread")
 
             await ctx.author.send(embed=embed_q)
             
-            try:
-                answer = await self.client.wait_for("message", check=Check, timeout=60)
-            except TimeoutError:
-                await ctx.author.send("Thread closed due to timeout")
+            answer = await self.client.wait_for("message", check=Check, timeout=timeout)
+            
+            if answer == None:
+                await ctx.author.send("Thread closed due to inactivity")
+                return
             
             value = answer.content
+
+            if(value.lower() == "cancel"):
+                await ctx.author.send("Thread cancelled")
+                return
 
             if(question["Format"] == "Number"):
                 try:
@@ -91,9 +92,13 @@ class Advertising(commands.Cog):
                   "excludeBannedUsers": True
                 }
                 r = requests.post("https://users.roblox.com/v1/usernames/users", data=body_)
-                data = r.json()
-                if len(list(data["data"])) == 0:
+                resp = r.json()
+                if(r.status_code != 200):
+                    await ctx.author.send(f"Internal server error: {r.status_code}\nPlease try again later")
+                    return
+                if len(list(resp["data"])) == 0:
                     await ctx.author.send("Must be a valid roblox user")
+                    print
                     await retry()
 
             if(value == None):
@@ -106,16 +111,13 @@ class Advertising(commands.Cog):
             else:
                 if(next_):
                     next_(post)
-                    return post
+                return post
                     
         
         try:
             with open(self.schema_path, "r") as f:
                 data = json.loads(f.read())
                 
-                # Big boss didn't like this one
-                await ctx.author.send("Hey! follow the steps below to start posting about your game")
-
                 questions_len = len(list(data["Questions"]))
 
                 start_index = 0
@@ -126,7 +128,7 @@ class Advertising(commands.Cog):
                     await ctx.author.send("Thread stopped it took too long")
 
         except FileNotFoundError:
-            await ctx.send("The schema for this question could not be found. Please contact a Moderator and show them this message")
+            await ctx.author.send("The schema for this question could not be found. Please contact a Moderator and show them this message")
 
 
     ## ____________ Events ____________ ##
