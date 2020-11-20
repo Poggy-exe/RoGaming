c875c8ff3a64297d1a725a51226211f8f2abc0d8
JanuZz <72662150+JanuZz@users.noreply.github.com>
2020-11-20 08:26:46 +0100

    Added comments

--- Commands/Post.py
+++ Commands/Post.py
@@ -20,27 +20,31 @@ class Advertising(commands.Cog):
     # Command
     @commands.command()
     async def post(self, ctx):
-
         
-        
+        ## The check to see if the message was sent in the right channel and/or by the same user as the origininal user
         def Check(context):
             return context.author == ctx.author and isinstance(context.channel, discord.channel.DMChannel)
 
+        ## The function where you have the information and you post it to the correct channel
         def post(post : object):
             print(post)
 
         async def Ask(ctx, data : object, index : int, post : object, max_index : int,timeout : int = 120, check : callable = Check, next_ : callable = None):
             
+            ## a lambda for sending an embed wihout a title
             quickEmbed = lambda message: discord.Embed(color=discord.Color.from_rgb(254,254,254)).add_field(name="\u200b",value=message)
             
+            ## lambda shortcut for retying the message if the user fails to give the correct informatiom
             retry = lambda: Ask(ctx,data, index, post, max_index,timeout,check, next_)
 
             question = list(data["Questions"])[index]
 
+            ## The main embed where the question is asked
             embed_q = discord.Embed(title = data["Title"] if index == 0 else "", description=data["Description"] if index == 0 else "", color=discord.Color.from_rgb(254,254,254))
 
             #print("Choises" in question)
 
+            ## line break variable because you cant write it in f-strings
             br = "\n"
 
             embed_q.add_field(name=question['Question'],value= f'`{question["Example"] + br if "Example" in question else "None provided"}`', inline=False)
@@ -52,18 +56,23 @@ class Advertising(commands.Cog):
 
             await ctx.author.send(embed=embed_q)
             
+            ## Wait for the message that the user will respond with
             answer = await self.client.wait_for("message", check=Check, timeout=timeout)
             
+            ## DOESN'T WORK error check to see if the user took too long
             if answer == None:
                 await ctx.author.send(embed=quickEmbed("Thread closed due to inactivity"))
                 return
             
             value = answer.content
 
+                                  
+            ## Check if the user cancelled the thread
             if(value.lower() == "cancel"):
                 await ctx.author.send(embed=quickEmbed("Thread cancelled"))
                 return
 
+            ## Check if there was any formatting associated with the question like number date or anything like that
             if(question["Format"] == "Number"):
                 try:
                     value = int(value)
@@ -89,6 +98,8 @@ class Advertising(commands.Cog):
                 except KeyError:
                     pass  
             elif(question["Format"] == "User"):
+    
+                ## Ask the roblox api if the user exists
                 body_ = {
                   "usernames": [
                     value
@@ -110,6 +121,7 @@ class Advertising(commands.Cog):
 
             post[question["Name"]] = value
 
+            ## If there are more questions do those otherwise return the collected information
             if(index+1 <= max_index):
                 await Ask(ctx, data, index+1, post, max_index, timeout, check, next_)
             else:
